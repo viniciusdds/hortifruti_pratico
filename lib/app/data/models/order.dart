0beacd4e7d8e2720a1de92612ca75d4461701f34
viniciusdds <viniciusdamasceno180@hotmail.com>
2024-07-05 15:40:28 -0300

    Aula 31

--- lib/app/data/models/order.dart
+++ lib/app/data/models/order.dart
@@ -1,5 +1,9 @@
+import 'package:app_hortifruti_pratico/app/data/models/address.dart';
+import 'package:app_hortifruti_pratico/app/data/models/order_product.dart';
 import 'package:app_hortifruti_pratico/app/data/models/order_status.dart';
+import 'package:app_hortifruti_pratico/app/data/models/payment_method.dart';
 import 'package:app_hortifruti_pratico/app/data/models/store.dart';
+import 'package:intl/intl.dart';
 
 class OrderModel {
 
@@ -7,6 +11,10 @@ class OrderModel {
   StoreModel store;
   String value;
   String deliveryCost;
+  AddressModel? address;
+  PaymentMethodModel? paymentMethod;
+
+  List<OrderProductModel> productList;
   List<OrderStatusModel> statusList;
   String? observation;
   DateTime createAt;
@@ -16,6 +24,10 @@ class OrderModel {
     required this.store,
     required this.value,
     required this.deliveryCost,
+    this.address,
+    this.paymentMethod,
+
+    required this.productList,
     required this.statusList,
     this.observation,
     required this.createAt,
@@ -26,6 +38,13 @@ class OrderModel {
     store: StoreModel.fromJson(json['estabelecimento']),
     value: json['valor'],
     deliveryCost: json['custo_entrega'],
+    address: json['endereco'] == null ? null : AddressModel.fromJson(json['endereco']),
+    paymentMethod: json['meio_pagamento'] == null ? null : PaymentMethodModel.fromJson(json['meio_pagamento']),
+    productList: json['produtos'] == null
+       ? []
+       : List<OrderProductModel>.from(
+            json['produtos'].map((product) => OrderProductModel.fromJson(product))
+         ),
     statusList: json['pedido_status'] == null
        ? []
        : List<OrderStatusModel>.from(
---
+++ lib/app/data/models/order_product.dart
@@ -0,0 +1,29 @@
+
+import 'package:app_hortifruti_pratico/app/data/models/product.dart';
+
+class OrderProductModel {
+
+  ProductModel product;
+  String quantity;
+  String value;
+  String? observation;
+
+  OrderProductModel({
+    required this.product,
+    required this.quantity,
+    required this.value,
+    this.observation
+  });
+
+  double get total => double.parse(value) * double.parse(quantity);
+
+  factory OrderProductModel.fromJson(Map<String, dynamic> json) => OrderProductModel(
+      product: ProductModel.fromJson(json['produto']),
+      quantity: json['quantidade'],
+      value: json['valor'],
+      observation: json['observacao']
+  );
+
+
+
+}
